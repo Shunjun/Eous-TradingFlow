936c0df07bb27f5180059fafeb3d9d9c1a77715d
Shunzi <toby.zsj@gmail.com>
2026-06-29 00:56:41 +0800

    feat: Remove legacy workflow editor redirect

--- apps/web/src/pages/(auth)/workflow/[id]/page.tsx
+++
@@ -1,6 +0,0 @@
-import { Navigate, useParams } from 'react-router-dom'
-
-export default function LegacyWorkflowEditorPage() {
-  const { id } = useParams()
-  return <Navigate to={id ? `/workflows/${id}/edit` : '/workflows'} replace />
-}
